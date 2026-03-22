dcfbcd14c370822c6e173a3f5a790f1f507a802a
Shubham Tyagi <tyagi.shubham177@gmail.com>
2026-03-22 16:16:54 +0530

    chore: remove app code and keep docs-first master

--- apps/web/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@potluck/config", "@potluck/contracts", "@potluck/ui"]
-};
-
-export default nextConfig;
